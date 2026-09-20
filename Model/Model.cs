e16737ad9ad360c9f409a59039ba91dd7e7abcec
anastasia <a.tugarinova@icloud.com>
2026-09-20 14:32:43 +0700

    initial commit

--- Model/Model.cs
+++ Model/Model.cs
@@ -11,8 +11,7 @@ namespace DecanatPRO
         public string Name { get; set; }
         public string Speciality { get; set; }
         public string Group { get; set; }
-        public int index { get; }
-
+        public int index;
         public Student(string name, string speciality, string group)
         {
             Name = name;    
