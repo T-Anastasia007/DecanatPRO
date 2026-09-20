85d6b7905a2594db4906fa6b03444051d1aa9348
chuunyai <chunyaim07@gmail.com>
2026-09-21 02:32:03 +0700

    Проверка на дурака рабочая версия

--- BusinessLogic/Logic.cs
+++ BusinessLogic/Logic.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -44,9 +45,46 @@ namespace DecanatPRO
                 Console.WriteLine("");
             }
         }
-        public void CheckOnDurak(string proverim)
+        public bool CheckOnDurak(string proverim, CheckMode cheсkmode)
         {
-
+            // proverim = proverim.Trim(); добавить в main, как и s = иван | char.ToUpper(s[0]) + s.Substring(1); s = Иван
+            if (proverim == null || proverim.Length > 50 || proverim.Length < 2 )
+            {
+                Console.WriteLine("Некорректная длина слова!");
+                return false;
+            }
+            
+            switch (cheсkmode)
+            {
+                case CheckMode.Letters:
+                    foreach (char a in proverim)
+                    {
+                        if ((a < 'a' || a > 'z') && (a < 'A' || a > 'Z') && (a < 'А' || a > 'я') && a != 'Ё' && a != 'ё') // подумать над англ буквами
+                        {
+                            Console.WriteLine("В имени/названии содержатся специальные символы, цифры или пробел!");
+                            return false;
+                        }
+                    }
+                    return true;
+                case CheckMode.SpecChars:
+                    foreach (char a in proverim)
+                    {
+                        if ((a < 33 || a > 90) && (a < 97 || a > 122) && (a < 'А' || a > 'я') && a != 'Ё' && a != 'ё')
+                        {
+                            Console.WriteLine("В имени/названии содержатся недопустимые символы!");
+                            return false;
+                        }
+                    }
+                    return true;
+                default:
+                    Console.WriteLine("Ошибка в проверке, попробуйте еще раз");
+                    return false;
+            }   
         }
     }
+}
+public enum CheckMode
+{
+    Letters,
+    SpecChars
 }
