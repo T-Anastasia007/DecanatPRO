050e63f176cbbea94caeb385d270f2906e91ac98
anastasia <a.tugarinova@icloud.com>
2026-09-20 14:28:05 +0700

    Добавьте файлы проекта.

---
+++ BusinessLogic/Logic.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace DecanatPRO
+{
+    public class Logic
+    {
+        public List<Student> students { get; set; } = new List<Student>();
+
+        public void AddStudent(string name, string speciality, string group)
+        {   
+
+            students.Add(new Student(name, speciality, group));
+            
+
+        }
+        public void DeleteStudent(int index)
+        {
+            students.RemoveAt(index);      
+        }
+        public void ShowTable() 
+        {
+
+        }
+        public void ShowGistogram()
+        {
+
+        }
+    }
+}
---
+++ Model/Model.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecanatPRO
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Speciality { get; set; }
+        public string Group { get; set; }
+        public int index { get; }
+
+        public Student(string name, string speciality, string group)
+        {
+            Name = name;    
+            Speciality = speciality;
+            Group = group;
+        }
+    }
+}
