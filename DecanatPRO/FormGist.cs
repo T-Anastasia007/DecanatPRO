aaa89dda94ec1587d961f3c39c5372846a4018a3
chuunyai <chunyaim07@gmail.com>
2026-09-23 19:56:46 +0300

    изи гистограммы

--- DecanatPRO/FormGist.cs
+++ DecanatPRO/FormGist.cs
@@ -7,14 +7,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace DecanatPRO
 {
     public partial class FormGist : Form
     {
+        private Logic _logic;
         public FormGist(Logic logic)
         {
             InitializeComponent();
+            _logic = logic;
+            chart1.Palette = ChartColorPalette.Excel;
+            chart1.Titles.Add("Кол-во студентов по настям");
+            var hist = _logic.ShowHistogram();
+            foreach (var a in hist)
+            {
+                Series series = chart1.Series.Add(a.Key);
+                series.Points.Add(a.Value);
+            }
         }
 
         private void buttonExit_Click(object sender, EventArgs e)
@@ -25,7 +36,7 @@ namespace DecanatPRO
 
         private void FormGist_Load(object sender, EventArgs e)
         {
-
+           
         }
     }
 }
